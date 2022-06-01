a7620b9f8fc40aadf8c9a6d66ca325d4393e3163
Ricardo Yaguachi <ricky081297@hotmail.com>
2022-06-01 13:28:54 -0500

    fix complete

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import axios from "axios";
 const API_URL = "https://api-shortener-url-with-domain.herokuapp.com/api/v1";
 
-async function shortener(url, domain) {
-  const response = await axios.post(`${API_URL}/shortener`, {
-    url: url,
-    domain: domain,
-  });
-  return response.data.url || "error to convert";
+async function shortener(link, domain) {
+  const response = await axios
+    .post(`${API_URL}/shortener`, {
+      url: link,
+      domain: domain,
+    })
+    .then((res) => {
+      return res.data.url;
+    })
+    .catch((error) => {
+      return error.response.data.error;
+    });
+  return response;
 }
 
 async function getUrl(shortUrl) {
-  const response = await axios.get(`${API_URL}/url/${shortUrl}`);
-  return response.data.url || "error to convert";
+  if(!shortUrl) return 'Link not found';
+  const response = await axios
+    .get(`${API_URL}/url/${shortUrl}`)
+    .then((res) => {
+      return res.data.url;
+    })
+    .catch((error) => {
+      return null;
+    });
+  return response;
 }
-
 module.exports = {
   shortener,
   getUrl,
---
+++ src/index.test.js
@@ -0,0 +1,67 @@
+import index from "../src/index";
+
+describe("should return a link", () => {
+  it("should return a link", async () => {
+    const link = "https://www.google.com";
+    const shortUrl = "01mz";
+    const result = await index.getUrl(shortUrl).then((url) => {
+      return url;
+    });
+    expect(result).toEqual(link);
+  });
+
+  it("should return void", async () => {
+    const shortUrl = "";
+    const result = await index.getUrl(shortUrl).then((url) => {
+      return url;
+    });
+    expect(result).toEqual("Link not found");
+  });
+});
+
+describe("should return a short link", () => {
+  it("should return a short link", async () => {
+    const link = "https://www.google.com";
+    const domain = "https://www.facebook.com";
+    const shortUrl = "/01mz";
+    const result = await index.shortener(link, domain).then((url) => {
+      return url;
+    });
+    expect(result).toEqual(domain + shortUrl);
+  });
+
+  it("should return a short link without domain", async () => {
+    const link = "https://www.google.com";
+    const domain = "https://shortener.com/01mz";
+    const result = await index.shortener(link).then((url) => {
+      return url;
+    });
+    expect(result).toEqual(domain);
+  });
+
+  it("should return Url is required with url invalid", async () => {
+    const link = "";
+    const domain = "https://www.facebook.com";
+    const result = await index.shortener(link, domain).then((url) => {
+      return url;
+    });
+    expect(result).toEqual("Url is required");
+  });
+
+  it("should return Domain is not valid with url invalid", async () => {
+    const link = "https://www.google.com";
+    const domain = "hello";
+    const result = await index.shortener(link, domain).then((url) => {
+      return url;
+    });
+    expect(result).toEqual("Domain is not valid");
+  });
+
+  it("should return Url is not valid without domain", async () => {
+    const link = "hello";
+    const result = await index.shortener(link).then((url) => {
+      return url;
+    });
+    expect(result).toEqual("Url is not valid");
+  });
+});
